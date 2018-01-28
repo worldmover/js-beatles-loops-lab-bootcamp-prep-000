df0baa988c2a462c9a23b3964291c162dd95dd9b
Christian Weiss <worldmover777@gmail.com>
2018-01-28 23:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
 var array=[]
+console.log("pepper")
  for(i=0;i<musicians.length+1;i++){
    console.log(i)
   array[i]=(musicians[i]+"plays"+instruments[i])
